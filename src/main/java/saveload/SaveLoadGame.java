033c61fbd7dd85dcf319de90bf37212079d2c3a2
Duc Thang Le <leductha@fel.cvut.cz>
2023-05-17 17:12:46 +0200

    LoadSave class initialized - TO DO

---
+++ src/main/java/saveload/SaveLoadGame.java
@@ -0,0 +1,71 @@
+package saveload;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import gamestates.Playing;
+import objects.Diamond;
+import objects.Heart;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class SaveLoadGame implements Serializable {
+    Playing playing;
+    Gson gson;
+
+    public SaveLoadGame(Playing playing) {
+        this.playing = playing;
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void save() {
+        try (FileWriter writer = new FileWriter("save.json")) {
+            GameData data = new GameData();
+            data.xPosition = playing.getPlayer().getXPosition();
+            data.yPosition = playing.getPlayer().getYPosition();
+
+            data.lives = playing.getPlayer().getLives();
+            data.diamonds = playing.getObjectManager().getNumberOfDiamondsToTake();
+
+            data.diamondsArray = playing.getObjectManager().getDiamonds();
+            data.heartsArray = playing.getObjectManager().getHearts();
+
+            data.pigsArray = playing.getEnemyManager().getPigs();
+
+            String json = gson.toJson(data);
+            writer.write(json);
+
+            System.out.println(data.diamonds);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void load() {
+        try {
+            String json = new String(Files.readAllBytes(Paths.get("save.json")));
+            GameData data = gson.fromJson(json, GameData.class);
+
+
+            // Set the player position
+            playing.getPlayer().getHitBox().x = data.xPosition;
+            playing.getPlayer().getHitBox().y = data.yPosition;
+
+            playing.getPlayer().setLives(data.lives);
+            playing.getObjectManager().setNumberOfDiamondsToTake(data.diamonds);
+
+            playing.getObjectManager().setDiamonds(new ArrayList<>(data.diamondsArray));
+            playing.getObjectManager().setHearts(new ArrayList<>(data.heartsArray));
+
+            playing.getEnemyManager().setPigs(new ArrayList<>(data.pigsArray));
+
+            System.out.println(data.diamonds);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
