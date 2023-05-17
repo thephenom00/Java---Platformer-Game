07f5a98e2fbf2fb2edee5ac57d5d3d66bc75533d
Duc Thang Le <leductha@fel.cvut.cz>
2023-05-17 17:11:03 +0200

    GameData class initialized

--- src/main/java/saveload/GameData.java
+++ src/main/java/saveload/GameData.java
@@ -1,4 +1,24 @@
 package saveload;
 
-public class GameData {
+import entities.Pig;
+import objects.Diamond;
+import objects.Heart;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameData implements Serializable {
+    // PLAYER DATA
+    float xPosition;
+    float yPosition;
+    int diamonds;
+    int lives;
+
+    // OBJECT DATA
+    ArrayList<Diamond> diamondsArray;
+    ArrayList<Heart> heartsArray;
+
+    // ENEMY DATA
+    ArrayList<Pig> pigsArray;
 }
